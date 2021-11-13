b622e957f47b8411bfd7c3b68cc247ca8a26bac0
arjun-a-aprendiza <94193240+arjun-a-aprendiza@users.noreply.github.com>
2021-11-13 15:27:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function setColorHoverListener() {
 /* Photos */
 
 function setRandomPhoto() {
-  let num = Math.floor(Math.random() * 13) + 1;
+  let num = Math.floor(Math.random() * 1) + 1;
   document.getElementById("propic").src = `./img/face${num}.jpg`;
 }
 
